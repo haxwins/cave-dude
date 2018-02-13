02ef85406796cdf494b83f8be961dc7f1d97466b
haxwins <haxregist@gmail.com>
2018-02-13 12:00:38 +0100

    alpha 0.1

---
+++ script.js
@@ -0,0 +1,95 @@
+let level1 = [
+  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  "x                            x",
+  "x                      xx    x",
+  "xxxx                         x",
+  "x        xx                  x",
+  "x                            x",
+  "x             xx             x",
+  "x                            x",
+  "x                            x",
+  "x                        c   x",
+  "xxxxzzzzxxxxxxxxxxxxxxxxxxxxxx",
+];
+function Level(plan){
+  this.width = plan[0].length;
+  this.height = plan.length;
+  this.board='';
+  this.playerPosition={};
+  for(let i=0,l=plan.length;i<l;i++){
+    this.board += '<tr>';
+    for(let j=0,k=plan[i].length;j<k;j++){
+      let temp=plan[i];
+      if(temp[j]=='x'){
+        this.board += "<th class='wall'></th>";
+      }
+      else if(temp[j]=='c'){
+        this.board += "<th class='player'></th>";
+        this.playerPosition.x = j*20;
+        this.playerPosition.y = i*20;
+      }
+      else if(temp[j]=='z'){
+        this.board += "<th class='lava'></th>";
+      }
+      else{
+        this.board += '<th></th>';
+      }
+    }
+    this.board += '</th>';
+  }
+}
+
+let keyPress = (e)=>{
+  if(e.key=="ArrowRight"){
+    plan1.playerPosition.x += 5;
+    renderMovment();
+  }
+  else if(e.key=="ArrowLeft"){
+    plan1.playerPosition.x -= 5;
+    renderMovment();
+  }
+  else if(e.key=="ArrowUp"){
+    plan1.playerPosition.y -= 5;
+    renderMovment();
+  }
+  else if(e.key=="ArrowDown"){
+    plan1.playerPosition.y += 5;
+    renderMovment();
+  }
+}
+//Blinking elements
+let blinkingSetup = ()=>{
+  let bool=true;
+  return (classMain,classBlink)=>{
+                let el = document.getElementsByClassName(classMain);
+                if(bool){
+                  for(let i=0; i<el.length;i++){
+                    el[i].classList.add(classBlink);
+                    bool = false;
+                  }
+                }
+                else{
+                  for(let i=0; i<el.length;i++){
+                    el[i].classList.remove(classBlink);
+                    bool = true;
+                  }
+                }
+              }
+}
+let blinkingPlayer = blinkingSetup();
+let blinkingLava = blinkingSetup();
+setInterval(()=>{blinking('player','player_blink')},1000);
+setInterval(()=>{blinking2('lava','lava_blink')},2000);
+//render player position
+let renderMovment = ()=>{
+  let playerDOM = document.getElementsByClassName("player");
+  playerDOM[0].style.left = plan1.playerPosition.x + 'px';
+  playerDOM[0].style.top = plan1.playerPosition.y + 'px';
+}
+renderMovment();
+//render level to DOM
+let plan1 = new Level(level1);
+let game=document.getElementById('game');
+game.innerHTML=plan1.board;
+//
+document.addEventListener("keypress",(e)=>{keyPress(e)},false);
